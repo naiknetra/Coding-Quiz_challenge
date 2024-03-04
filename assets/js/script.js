36f06e48324f2785458ea3aec63867fa923e8b98
naiknetra <naiknetra5@gmail.com>
2024-03-03 19:44:36 -0500

    contents added to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,35 @@
 //set timer
 //selects element by class
-// var timeEl = document.queryselector(".time");
+var timeEl = document.querySelector(".time");
 
-// // Selects element by id
-// var mainEl = document.getElementById("main");
+//  Selects element by id
+//var mainEl = document.getElementById("main");
 
-// var secondleft = 75;
-//var arrBtnQ1 = document.querySelectorAll(".btnQ1");
+var secondsLeft = 75;
+function setTime() {
+    // Sets interval in variable
+    //var timerInterval = setInterval(function() {...},1000(miliseconds));
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        timeEl.textContent = "Time: " + secondsLeft;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        if (secondsLeft === 0) {
+            // Stops execution of action at set interval
+            clearInterval(timerInterval);
+            for (var i = 0; i < allSections.length; i++) {
+                allSections[i].setAttribute("class", "hide");
+               
+            }
 
 
+            // shows the initials section when timer runs out
+            showNextSection(3, "You are out of time");
 
+        }
 
+    }, 1000);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 
 
@@ -56,11 +43,35 @@ var allSections = document.querySelectorAll("section");
 //showes the first question section
 function showQuestionOne(event) {
     event.preventDefault();
-   
+    setTime();
+
     showNextSection(1);
 }
 
 btnStart.addEventListener("click", showQuestionOne);
+
+
+
+
+//Shows the section whose index number is passed as a parameter and hides the previous section
+
+function showNextSection(i, result) {
+    allSections[i].setAttribute("class", "show");
+    allSections[i - 1].setAttribute("class", "hide");
+    var eleSpanResult = allSections[i].querySelector(".qResult");
+    eleSpanResult.innerText = result;
+}
+
+for (var i = 0; i < listBtnQ1.length; i++) {
+    listBtnQ1[i].addEventListener("click", checkAnswer1);
+}
+
+//showes the second question section
+
+function showQuestionTwo(event) {
+    event.preventDefault();
+    showNextSection(2);
+}
 
 var isQ1AnswerCorrect;
 //checks if answer is Correct or Wrong
@@ -73,29 +84,9 @@ function checkAnswer1(event) {
     }
     else {
         isQ1AnswerCorrect = "Wrong!";
+        secondsLeft = secondsLeft-10;
     }
     showNextSection(2, isQ1AnswerCorrect);
-}
-
-//Shows the section whose index number is passed as a parameter and hides the previous section
-
-function showNextSection(i ,result) {    
-    allSections[i].setAttribute("class", "show");
-    allSections[i - 1].setAttribute("class", "hide");
-    var eleSpanResult = allSections[i].querySelector(".qResult");
-    eleSpanResult.innerText = result;
-}
-
-for (var i = 0 ; i< listBtnQ1.length ; i++)
-{
-listBtnQ1[i].addEventListener("click", checkAnswer1);
-}
-
-//showes the second question section
-
-function showQuestionTwo(event) {
-    event.preventDefault();   
-    showNextSection(2);
 }
 
 var isQ2AnswerCorrect;
@@ -109,13 +100,13 @@ function checkAnswer2(event) {
     }
     else {
         isQ2AnswerCorrect = "Wrong!";
+        secondsLeft = secondsLeft-10;
     }
     showNextSection(3, isQ2AnswerCorrect);
 }
 
-for (var i = 0 ; i< listBtnQ2.length ; i++)
-{
-listBtnQ2[i].addEventListener("click", checkAnswer2);
+for (var i = 0; i < listBtnQ2.length; i++) {
+    listBtnQ2[i].addEventListener("click", checkAnswer2);
 }
 
 
